380b76f2aec613af01b684081507ca30fba1f040
unknown <104025@INGURTC700038.AMERICAS.GLOBAL.NTTDATA.COM>
2016-12-23 11:22:22 +0530

    ADD PROJECT

---
+++ work-chain/work-chain/js/main.js
@@ -0,0 +1,35 @@
+$(function () {
+	stickyHeader();
+
+	//megaEqualHeight();
+
+	//Window scroll funcation call
+	$(window).scroll(function () {
+		//Calls sticky header for desktop
+		stickyHeader();
+	});
+
+	//Resize same height on window resize
+	var resize;
+	$(window).resize(function() {
+		$('.layer-grid .row .item').css("height", "auto");
+		gridEqualHeight();
+		//$('.dropdown-menu.mega-dropdown-menu li.col-sm-3').css("height", "auto");
+		//megaEqualHeight();
+	});
+
+	//Adds class to parent when nav is toggled
+	$('.navbar-header button.navbar-toggle').click(function() {
+		$('.navbar.navbar-default').toggleClass('open');
+	});
+
+	//Sticky header function for nav
+	function stickyHeader() {
+		if ($(this).scrollTop() > 100) {
+			$('header').addClass('fixed').fadeIn();
+		} else {
+			$('header').removeClass('fixed').fadeIn();
+		}
+  	}
+
+	});
